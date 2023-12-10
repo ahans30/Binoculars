fe4554329b233111d7e0b6e74e8147c6070f080e
ahans1 <abhimanyu.hans30@gmail.com>
2023-12-10 00:01:33 -0500

    Updated dir tree

--- binoculars/main.py
+++
@@ -1,9 +0,0 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import sys
-
-interpreter = sys.executable  # get the python interpretor path
-
-print(interpreter)
