7b1b1ed881cb7ec0e7ff0d2e0cc2a0de3b891711
ahans1 <abhimanyu.hans30@gmail.com>
2024-01-14 10:59:11 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Binoculars',
+    version='0.0.10',
+    packages=find_packages(),
+    url='https://github.com/ahans30/Binoculars',
+    license='',
+    author='Authors of "Binoculars: Zero-Shot Detection of LLM-Generated Text"',
+    author_email='ahans1@umd.edu',
+    description='A language model generated text detector.',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=open("requirements.txt", "r", encoding="utf-8").read().splitlines(),
+)
