42c77be2ac6a01061c89205271488fa00f3e10e0
ahans1 <abhimanyu.hans30@gmail.com>
2024-01-20 08:28:35 -0500

    Added default config

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 huggingface_config = {
     # Only required for private models from Huggingface (e.g. LLaMA models)
-    "TOKEN": os.environ["HF_TOKEN"] if "HF_TOKEN" in os.environ else None,
+    "TOKEN": os.environ.get("HF_TOKEN", None)
 }
